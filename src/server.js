4fd78f591a4afdba805b58578763bc57d6a03006
jhonny perez <jhonnyperez@worldofficeonlne.com>
2021-01-19 10:55:55 -0500

    Heroku

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+var favicon = require('serve-favicon')
 const path = require('path');
 
 
 app.use(express.static(__dirname+'/dist/angularOffline'));
+app.use(favicon(__dirname + '/dist/angularOffline/favicon.ico'));
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname +'/dist/angularOffline/index.html'));
 });
