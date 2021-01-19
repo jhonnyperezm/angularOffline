9708d78fa146423c9ec95b964e0192804abbbc39
jhonny perez <jhonnyperez@worldofficeonlne.com>
2021-01-19 10:52:45 -0500

    Heroku

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
+const express = require('express');
 const app = express();
+const path = require('path');
+
 
 app.use(express.static(__dirname+'/dist/angularOffline'));
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/angularOffline/index.html'));
+  res.sendFile(path.join(__dirname +'/dist/angularOffline/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
+
